e16588f777721be11eb9893c64cef857224d0a37
tomtommahout <tomgrace@gmail.com>
2024-05-05 19:38:29 +1200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
 from machine import Pin
 
-firmware_url = "https://github.com/tomtommahout/test_ota"
+firmware_url = "https://github.com/tomtommahout/test_ota/"
 
 ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "test.py")
 ota_updater.download_and_install_update_if_available()
