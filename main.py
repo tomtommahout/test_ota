185dfc9e0666283423774ee88d199c6e367ba6c4
tomtommahout <tomgrace@gmail.com>
2024-05-05 19:41:06 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
-from machine import Pin
+from machine import Pin, Timer
 
 firmware_url = "https://github.com/tomtommahout/test_ota/"
 
 ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "test.py")
 ota_updater.download_and_install_update_if_available()
 
-
 led = Pin(25, Pin.OUT)
+timer = Timer()
 
-led.toggle()
+def blink(timer):
+    led.toggle()
+
+timer.init(freq=2.5, mode=Timer.PERIODIC, callback=blink)
